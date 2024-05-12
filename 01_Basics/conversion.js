58317c298af8ef956944c189a0a304c656e9c3e6
Sourav kumar Arya <soravarya175@gmail.com>
2024-05-12 04:50:28 +0000

    uoToConversion.js

---
+++ 01_Basics/conversion.js
@@ -0,0 +1,18 @@
+
+console.log(+true);
+console.log(+"");
+
+
+console.log(null > 0);   // false
+console.log(null == 0);  // false
+console.log(null >= 0); // true , because in comaparion(> and < ) "null" is converted into number 
+
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined);
+
+// ===
+
+console.log("2" === 2);  // false , because === compare the data type also 
