e6011efafadc369b58f3b6c6a5330b76b9a72655
Sourav kumar Arya <soravarya175@gmail.com>
2024-05-12 05:45:13 +0000

    dataTypes_summary1

--- 01_Basics/dataTypes_summary.js
+++ 01_Basics/dataTypes_summary.js
@@ -23,3 +23,31 @@ const myFunction = function(){
     console.log("Hello, mr sourav Arya");
 }
 myFunction();
+
+console.log("-------------MEMORY-------------------");
+console.log("");
+
+// Stack(Primitve), 
+
+let name = "SouravArya";
+let name1 = name;
+
+name1 = "Raman";
+console.log(name);
+console.log(name1);
+console.log(name);
+
+//Heap(Non-Primitive)
+
+let user1 = {
+    name : "sourav",
+    department : "development"
+}
+console.log(user1);
+
+let user2 = user1;
+user2.name = "raman";
+console.log(user1);
+
+
+console.log("");
