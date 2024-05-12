c786f257eaeae8614f40ce1587280b730dcc6b78
Sourav kumar Arya <soravarya175@gmail.com>
2024-05-12 05:18:28 +0000

    DATA-TYPE_SUMMARY

---
+++ 01_Basics/dataTypes_summary.js
@@ -0,0 +1,25 @@
+
+// PRIMITIVE 7 Types :- String, boolean, Number, null, undefined, symbol, BigInt
+
+// NON-PRIMITIVE :- Reference :- Array, Object, Function 
+
+// Array
+console.log("---------------ARRAY-----------------");
+
+const heros = ["shaktiman", "nagraj", "souravArya"];
+console.log(heros);
+
+// objects
+console.log("---------------OBJECTS-----------------");
+let founder = {
+    name : "sourav",
+    roll : 12345,
+}
+console.log(founder);
+
+// Function 
+console.log("-------------FUNCTIONS-------------------");
+const myFunction = function(){
+    console.log("Hello, mr sourav Arya");
+}
+myFunction();
